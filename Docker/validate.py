9e801436473c0c29af054be3f237a035e66740c1
verena <vpchung@users.noreply.github.com>
2021-07-27 23:28:09 -0700

    update validation script

--- Docker/validate.py
+++ Docker/validate.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 """Validation script for Task 1.
 
-Predictions file must be a NIfTI file (*.nii.gz), where filename must
-match with the goldstandard's naming format.
+Predictions file must be a tarball or zipped archive of NIfTI files
+(*.nii.gz). Each NIfTI file must have an ID in its filename.
 """
 import argparse
 import json
+import tarfile
+import zipfile
 
 import nibabel as nib
 
@@ -22,12 +24,41 @@ def get_args():
     return parser.parse_args()
 
 
-def validate_img(img):
-    """Validate image file.
+def get_images(submission):
+    """Untar or unzip submission file."""
+    if zipfile.is_zipfile(submission):
+        with zipfile.ZipFile(submission) as zip_ref:
+            imgs = zip_ref.namelist()
+    elif tarfile.is_tarfile(submission):
+        with tarfile.TarFile(submission) as tar_ref:
+            imgs = tar_ref.getnames()
+    else:
+        imgs = []
+    return imgs
 
-    TODO: add validation checks.
-    """
-    return []
+
+def validate_file_format(imgs):
+    """Check that all files are NIfTI files (*.nii.gz)."""
+    error = []
+    if not all(f.endswith(".nii.gz") for f in imgs):
+        error = ["Not all files in the archive are NIfTI files (*.nii.gz)."]
+    return error
+
+
+def validate_filenames(imgs):
+    """Check that there is a NIfTI file for every scan ID (001-166)."""
+    missing_ids = []
+    scan_ids = range(1, 167)
+    for i in scan_ids:
+        scan_id = f"{i:03d}"
+        if not any(scan_id in f for f in imgs):
+            missing_ids.append(scan_id)
+
+    error = []
+    if missing_ids:
+        error = [("Missing NIfTI file for the following IDs: "
+                  f"{', '.join(missing_ids)}")]
+    return error
 
 
 def main():
@@ -35,16 +66,15 @@ def main():
     args = get_args()
 
     invalid_reasons = []
-    try:
-        img = nib.load(args.predictions_file)
-    except nib.filebasedimages.ImageFileError:
-        invalid_reasons.append(
-            ("Submitted file is not an acceptable neuroimaging file format. "
-             "Double-check that you are submitting a NIfTI file (*.nii.gz).")
-        )
-    else:
-        invalid_reasons.extend(validate_img(img))
 
+    imgs = get_images(args.predictions_file)
+    if imgs:
+        invalid_reasons.extend(validate_file_format(imgs))
+        invalid_reasons.extend(validate_filenames(imgs))
+    else:
+        invalid_reasons.append(
+            "Submission must be a tarball or zipped archive of NIfTI files."
+        )
     status = "INVALID" if invalid_reasons else "VALIDATED"
 
     with open(args.output, "w") as out:
