e57444afb8dd90ef6425efdaf5eae380880ee3c3
verena <9377970+vpchung@users.noreply.github.com>
2021-08-26 04:26:04 -0700

    fix bugs

--- run_docker.py
+++ run_docker.py
@@ -11,9 +11,9 @@ import docker
 import synapseclient
 
 
-def create_log_file(log_filename, log_text=None):
+def create_log_file(log_filename, log_text=None, mode="w"):
     """Create log file"""
-    with open(log_filename, 'w') as log_file:
+    with open(log_filename, mode) as log_file:
         if log_text is not None:
             if isinstance(log_text, bytes):
                 log_text = log_text.decode("utf-8")
@@ -95,99 +95,108 @@ def main(syn, args):
 
     print(getpass.getuser())
 
+    # Create a logfile to catch stdout/stderr from the Docker runs.
+    print("creating logfile")
+    log_filename = args.submissionid + "_log.txt"
+    open(log_filename, 'w').close()
+    warnings = []
+
     # Get Docker image to run and volumes to be mounted.
     docker_image = args.docker_repository + "@" + args.docker_digest
     output_dir = os.getcwd()
+    input_dir = args.input_dir
 
     # For the input directory, there will be a different case folder per
     # Docker run, e.g. /path/to/BraTS2021_00001, /path/to/BraTS2021_00013,
     # etc. In total, there will be 5 Docker runs for the validation data,
     # 500 for the testing data.
-    for root, sub_dirs, _ in os.walk(args.input_dir):
-        for sub_dir in sub_dirs:
-            case_folder = os.path.join(root, sub_dir)
-            case_id = case_folder[-5:]
+    for sub_dir in os.listdir(input_dir):
+        case_folder = os.path.join(input_dir, sub_dir)
+        case_id = case_folder[-5:]
 
-            print("mounting volumes")
+        print("mounting volumes")
+        # Specify the input directory with 'ro' permissions, output with
+        # 'rw' permissions.
+        # mounted_volumes = {output_dir: '/output:rw',
+        #                    case_folder: '/input:ro'}
 
-            # Specify the input directory with 'ro' permissions, output with
-            # 'rw' permissions.
-            # mounted_volumes = {output_dir: '/output:rw',
-            #                    case_folder: '/input:ro'}
+        # FOR NOW: stress-test with these mounted directories, which are
+        #          expected in last year's models.
+        mounted_volumes = {
+            output_dir: '/app/data/results/:rw',
+            case_folder: '/app/data:rw'
+        }
 
-            # FOR NOW: stress-test with these mounted directories, which are
-            #          expected in last year's models.
-            mounted_volumes = {
-                output_dir: '/data/results:rw',
-                case_folder: '/data:rw'
-            }
+        # Format the mounted volumes so that Docker SDK can understand.
+        all_volumes = [output_dir, case_folder]
+        volumes = {}
+        for vol in all_volumes:
+            volumes[vol] = {'bind': mounted_volumes[vol].split(":")[0],
+                            'mode': mounted_volumes[vol].split(":")[1]}
 
-            # Format the mounted volumes so that Docker SDK can understand.
-            all_volumes = [output_dir, case_folder]
-            volumes = {}
-            for vol in all_volumes:
-                volumes[vol] = {'bind': mounted_volumes[vol].split(":")[0],
-                                'mode': mounted_volumes[vol].split(":")[1]}
+        # Run the Docker container in detached mode and with access
+        # to the GPU, also making note of the start time.
+        container_name = f"{args.submissionid}_case{case_id}"
+        print(f"running container: {container_name}")
+        start_time = time.time()
+        time_elapsed = 0
+        try:
+            container = client.containers.run(docker_image,
+                                              "python3 DeepSCAN_BRATS_2020.py -l",  # TODO: remove after stress test
+                                              detach=True,
+                                              volumes=volumes,
+                                              name=container_name,
+                                              network_disabled=True,
+                                              stderr=True,
+                                              runtime="nvidia")
+        except docker.errors.APIError as err:
+            container = None
+            remove_docker_container(container_name)
+            errors = str(err) + "\n"
+        else:
+            errors = ""
 
-            # Run the Docker container in detached mode and with access
-            # to the GPU, also making note of the start time.
-            print("running container")
-            start_time = time.time()
-            time_elapsed = 0
-            try:
-                container_name = f"{args.subissionid}_case{case_id}"
-                container = client.containers.run(docker_image,
-                                                  detach=True,
-                                                  volumes=volumes,
-                                                  name=container_name,
-                                                  network_disabled=True,
-                                                  stderr=True,
-                                                  runtime="nvidia")
-            except docker.errors.APIError as err:
-                remove_docker_container(args.submissionid)
-                errors = str(err) + "\n"
-
-            # Create a logfile to catch stdout/stderr from the Docker run.
-            print("creating logfile")
-            log_filename = args.submissionid + "_log.txt"
-            open(log_filename, 'w').close()
-
-            # If container is running, monitor the time elapsed -- if it
-            # exceeds the runtime quota, stop the container. Logs should
-            # also be captured every 60 seconds.  Remove the container
-            if container is not None:
-                while container in client.containers.list():
-                    time_elapsed = time.time() - start_time
-                    if time_elapsed > args.runtime_quota:
-                        container.stop()
-                        break
-
-                    log_text = container.logs()
-                    create_log_file(log_filename, log_text=log_text)
-                    store_log_file(syn, log_filename,
-                                   args.parentid, store=args.store)
-                    time.sleep(60)
-
-                # Note the reason for container exit in the logs if time
-                # limit is reached.
+        # If container is running, monitor the time elapsed -- if it
+        # exceeds the runtime quota, stop the container. Logs should
+        # also be captured every 60 seconds.  Remove the container
+        if container is not None:
+            while container in client.containers.list():
+                time_elapsed = time.time() - start_time
                 if time_elapsed > args.runtime_quota:
-                    log_text = (f"Time limit of {args.runtime_quota}s reached"
-                                f"for case {case_id} - no output generated.")
+                    container.stop()
+                    break
 
-                else:
-                    # Must run again to make sure all the logs are captured
-                    log_text = container.logs()
-
+                log_text = container.logs()
                 create_log_file(log_filename, log_text=log_text)
                 store_log_file(syn, log_filename,
                                args.parentid, store=args.store)
-                container.remove()
+                time.sleep(60)
 
-            statinfo = os.stat(log_filename)
-            if statinfo.st_size == 0:
-                create_log_file(log_filename, log_text=errors)
-                store_log_file(syn, log_filename,
-                               args.parentid, store=args.store)
+            # Note the reason for container exit in the logs if time
+            # limit is reached.
+            if time_elapsed > args.runtime_quota:
+                warnings.append(f"Time limit of {args.runtime_quota}s reached "
+                                f"for case {case_id} - no output generated.")
+
+            # Must run again to make sure all the logs are captured
+            log_text = container.logs()
+            create_log_file(log_filename, log_text=log_text)
+            store_log_file(syn, log_filename,
+                           args.parentid, store=args.store)
+            container.remove()
+
+        statinfo = os.stat(log_filename)
+        if statinfo.st_size == 0 and errors:
+            create_log_file(log_filename, log_text=errors)
+            store_log_file(syn, log_filename,
+                           args.parentid, store=args.store)
+    if warnings:
+        warnings_text = ("\n\nWarnings:\n========="
+                         "\n".join(warnings))
+        create_log_file(log_filename, log_text=warnings_text, mode="a")
+        store_log_file(syn, log_filename,
+                       args.parentid, store=args.store)
+
     print("finished training")
     remove_docker_image(docker_image)
 
@@ -218,7 +227,7 @@ if __name__ == '__main__':
     parser.add_argument("-c", "--synapse_config", required=True,
                         help="credentials file")
     parser.add_argument("-rt", "--runtime_quota",
-                        help="runtime quota in seconds")
+                        help="runtime quota in seconds", type=int)
     parser.add_argument("--store", action='store_true',
                         help="to store logs")
     parser.add_argument("--parentid", required=True,
@@ -226,5 +235,5 @@ if __name__ == '__main__':
     parser.add_argument("--status", required=True, help="Docker image status")
     args = parser.parse_args()
     syn = synapseclient.Synapse(configPath=args.synapse_config)
-    syn.login()
+    syn.login(silent=True)
     main(syn, args)
