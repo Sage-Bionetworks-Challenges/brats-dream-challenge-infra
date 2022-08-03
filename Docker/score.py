081a75fbeeee16fe316c6235353d8fc27e43a90f
verena <9377970+vpchung@users.noreply.github.com>
2022-08-03 00:39:01 -0700

    split scores by dataset

--- Docker/score.py
+++ Docker/score.py
@@ -50,7 +50,7 @@ def run_captk(path_to_captk, pred, gold, tmp):
     subprocess.check_call(cmd)
 
 
-def extract_metrics(tmp, scan_id):
+def extract_metrics(tmp, scan_id, prefix):
     """Get scores for three regions: ET, WT, and TC.
 
     Metrics wanted:
@@ -67,28 +67,30 @@ def extract_metrics(tmp, scan_id):
                        "Precision"])
         .filter(items=["ET", "WT", "TC"], axis=0)
         .reset_index()
-        .assign(scan_id=f"BraTS2021_{scan_id}")
+        .assign(scan_id=f"{prefix}_{scan_id}")
         .pivot(index="scan_id", columns="Labels")
     )
     res.columns = ["_".join(col).strip() for col in res.columns]
     return res
 
 
-def score(parent, pred_lst, captk_path, tmp_output="tmp.csv"):
+def score(parent, pred_lst, captk_path, dataset="BraTS2021", tmp_output="tmp.csv"):
     """Compute and return scores for each scan."""
     scores = []
-    for pred in pred_lst:
+    filtered_preds = [f for f in pred_lst
+                      if os.path.split(f)[1].startswith(dataset)]
+    for pred in filtered_preds:
         scan_id = pred[-12:-7]
-        gold = os.path.join(parent, f"BraTS2021_{scan_id}_seg.nii.gz")
+        gold = os.path.join(parent, f"{dataset}_{scan_id}_seg.nii.gz")
         try:
             run_captk(captk_path, pred, gold, tmp_output)
-            scan_scores = extract_metrics(tmp_output, scan_id)
+            scan_scores = extract_metrics(tmp_output, scan_id, dataset)
             os.remove(tmp_output)  # Remove file, as it's no longer needed
         except subprocess.CalledProcessError:
             # If no output found, give penalized scores.
             scan_scores = (
                 pd.DataFrame({
-                    "scan_id": [f"BraTS2021_{scan_id}*"],
+                    "scan_id": [f"{dataset}_{scan_id}*"],
                     "Dice_ET": [0], "Dice_TC": [0], "Dice_WT": [0],
                     "Hausdorff95_ET": [374], "Hausdorff95_TC": [374],
                     "Hausdorff95_WT": [374], "Sensitivity_ET": [0],
@@ -103,15 +105,7 @@ def score(parent, pred_lst, captk_path, tmp_output="tmp.csv"):
     return pd.concat(scores).sort_values(by="scan_id")
 
 
-def main():
-    """Main function."""
-    args = get_args()
-    preds = utils.unzip_file(args.predictions_file)
-    golds = utils.unzip_file(args.goldstandard_file)
-
-    dir_name = os.path.split(golds[0])[0]
-    results = score(dir_name, preds, args.captk_path)
-
+def compile_and_upload(syn, parent, results, prefix=""):
     # Get number of segmentations predicted by participant, number of
     # segmentation that could not be scored, and number of segmentations
     # that were scored.
@@ -119,6 +113,7 @@ def main():
     flagged_cases = int(results.reset_index().scan_id.str.count(r"\*").sum())
     cases_evaluated = cases_predicted - flagged_cases
 
+    # Aggregate scores.
     results.loc["mean"] = results.mean()
     results.loc["sd"] = results.std()
     results.loc["median"] = results.median()
@@ -126,18 +121,37 @@ def main():
     results.loc["75quantile"] = results.quantile(q=0.75)
 
     # CSV file of scores for all scans.
-    results.to_csv("all_scores.csv")
+    filename = f"{prefix}all_scores.csv"
+    results.to_csv(filename)
+    csv = synapseclient.File(filename, parent=parent)
+    csv = syn.store(csv)
+    return {
+        f"{prefix}cases_evaluated": cases_evaluated,
+        f"{prefix}submission_scores": csv.id
+    }
+
+
+def main():
+    """Main function."""
+    args = get_args()
+    preds = utils.unzip_file(args.predictions_file)
+    golds = utils.unzip_file(args.goldstandard_file)
+    dir_name = os.path.split(golds[0])[0]
+
+    scores = score(dir_name, preds, args.captk_path)
+    ssa_scores = score(dir_name, preds, args.captk_path, dataset="BraTS_SSA")
+
     syn = synapseclient.Synapse(configPath=args.synapse_config)
     syn.login(silent=True)
-    csv = synapseclient.File("all_scores.csv", parent=args.parent_id)
-    csv = syn.store(csv)
+    results = compile_and_upload(syn, args.parent_id, scores)
+    ssa_results = compile_and_upload(syn, args.parent_id,
+                                     ssa_scores, prefix="ssa_")
 
     # Results file for annotations.
     with open(args.output, "w") as out:
-        res_dict = {**results.loc["mean"].to_dict(),
-                    "cases_evaluated": cases_evaluated,
-                    "submission_scores": csv.id,
-                    "submission_status": "SCORED"}
+        res_dict = {  # **results.loc["mean"].to_dict(),
+            **results, **ssa_results,
+            "submission_status": "SCORED"}
         res_dict = {k: v for k, v in res_dict.items() if not pd.isna(v)}
         out.write(json.dumps(res_dict))
 
